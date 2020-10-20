c0d486c24319a65e5220c9495c4f5aff85896858
samertaieb <taieb.samer@outlook.fr>
2020-10-20 12:45:52 +0100

    first commit

---
+++ calcul.js
@@ -0,0 +1,12 @@
+function number7() {
+document.getElementById("bot7").value=document.getElementById("bot7") +1;
+var a = parseInt(bot7);
+console.log(a);
+return a;
+}
+// function number8() {
+// var bot8=document.getElementById("bot8").value;
+// var b = parseInt(bot8);
+// console.log(b);
+// return b;
+// }
